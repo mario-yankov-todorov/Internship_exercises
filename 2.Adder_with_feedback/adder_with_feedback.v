a5e351ab0cebf3ee7050b39d38593b603f044005
Mario Todorov <mario.yankov.todorov@gmail.com>
2020-12-31 16:09:07 +0200

    Edit files

--- 2.Adder_with_feedback/adder_with_feedback.v
+++ 2.Adder_with_feedback/adder_with_feedback.v
@@ -6,7 +6,23 @@ module adder_with_feedback
         output   [16    -1:0]    out       ,
         input                    reset     , 
         input                    clock   
-        // ------------------------------------------------------------------------------
      );
+        // ------------------------------------------------------------------------------
+        // Wire and register
+        wire     [16    -1:0]    sum       ;
+		  reg      [16    -1:0]    register  ;
+        // Assignments
+        assign   out    = register         ;
+        assign   sum    = in + register    ;
+        
+		  // ------------------------------------------------------------------------------
+        // Procedural block
+        always   @    (posedge clock)
+            begin
+                if  (reset      == 1)
+                    register    <= 0       ;
+                else
+                    register    <= sum     ;
+            end
 		  
 endmodule
