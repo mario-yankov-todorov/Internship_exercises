c0dcff38a7bef4f6e1ac9e632bae50e7b76337cd
Mario Todorov <mario.yankov.todorov@gmail.com>
2021-01-07 19:16:57 +0200

    Edit files

--- 2.Adder_with_feedback/testbench.v
+++ 2.Adder_with_feedback/testbench.v
@@ -9,7 +9,66 @@ module testbench                         ;
         reg                    reset     ;
         reg                    clk       ;
 		  
-		  `include "check_the_values.sv"
+        // ---------------------------------------------------------------
+            // Create a task to check the values 		  
+            task print_the_values                               ;
+	 
+                input     [4    -1:0]    print_number           ; 
+	 
+                input                    clk_expected           ;
+                input                    reset_expected         ;
+                input    [16    -1:0]    in_expected            ;
+                input    [16    -1:0]    out_expected           ;
+		  
+                input                    clk_actual             ;
+                input                    reset_actual           ;
+                input    [16    -1:0]    in_actual              ;
+                input    [16    -1:0]    out_actual             ;
+		  
+		 
+                    if  (   clk_actual   !=   clk_expected    || 
+                            reset_actual !=   reset_expected  ||
+                            in_actual    !=   in_expected     || 
+                            out_actual   !=   out_expected        )  begin
+				  
+                            $display ()                                        ;
+                            $display ("%0d. ERROR: the values were wrong!"  ,
+                                      print_number)                            ;
+                            $display ()                                        ;
+                            $display ("   Expected: clk = %0d  reset = %0d" ,
+                                      clk_expected, reset_expected)            ; 
+                            $display ("             in  = %0d  out   = %0d" ,
+                                      in_expected, out_expected)               ;
+                            $display ()                                        ;
+                            $display ("   Actual:   clk = %0d, reset = %0d" , 
+                                      clk_actual, reset_actual)                ;
+                            $display ("             in  = %0d, out   = %0d" ,
+                                      in_actual, out_actual)                   ;
+                            $display ()                                        ;
+						
+                            end 
+					  
+                    else    begin
+				  
+                            $display ()                                        ;
+                            $display ("%0d. The values were correct!"       ,
+                                      print_number)                           ;
+                            $display ()                                        ;
+                            $display ("   Expected: clk = %0d  reset = %0d" ,
+                                      clk_expected,  reset_expected)           ; 
+                            $display ("             in  = %0d  out   = %0d" ,
+                                      in_expected,  out_expected)              ; 
+                            $display ()                                        ;
+                            $display ("   Actual:   clk = %0d, reset = %0d" , 
+                                      clk_actual, reset_actual)                ;
+                            $display ("             in  = %0d, out   = %0d" ,
+                                      in_actual, out_actual)                   ;
+                            $display ()                                        ;
+						
+                            end
+				  
+            endtask
+				
 		  
         // ---------------------------------------------------------------
         // Create instance of adder_with_feedback module
@@ -88,15 +147,41 @@ module testbench                         ;
           @     (out)                ; //     out   =   0(out) +   5(in)
                                        //     out   =   5
          // --------------------------------------------------------------
-		    begin
-		        print_the_values(1, 1, 0, 5, 5, clk, reset, in, out);
-			 end
-         
+             begin
+                 print_the_values(1, 1, 0, 5, 5, clk, reset, in, out)    ;
+             end      
+         // --------------------------------------------------------------
+			
+         #100
+         // --------------------------------------------------------------		  
+          //  At time  200    ! clk = 1 !       !!    posedge    !!
+                                       //  !! start procedural block !!    
+          in       =    16'd    5    ; //     reset =   0   in =   5
+          // wait for the output        // -------------------------------	 
+          @     (out)                ; //     out   =   5(out) +   5(in)
+                                       //     out   =  10
+         // --------------------------------------------------------------
+             begin
+                 print_the_values(2, 1, 0, 5, 10, clk, reset, in, out)   ;
+             end      
          // --------------------------------------------------------------
 	  
-         #100 // At time 200 - stop the tests 
+         #100
+         // --------------------------------------------------------------		  
+          //  At time  300    ! clk = 1 !       !!    posedge    !!
+                                       //  !! start procedural block !!    
+          in       =    16'd  200    ; //     reset =   0   in =  200
+          // wait for the output        // -------------------------------	 
+          @     (out)                ; //     out   =   10(out) + 200(in)
+                                       //     out   =  210
+         // --------------------------------------------------------------
+             begin
+                 print_the_values(3, 1, 0, 200, 210, clk, reset, in, out);
+             end      
+         // --------------------------------------------------------------
+         #100 // At time 400 - stop the tests 
                                                  
-          $finish                                                    ;
+          $finish                                                        ;
 	 
          end
 		  
